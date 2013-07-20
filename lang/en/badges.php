chapunchi/moodle

MDL-39864: First attempt to move badge criteria into a plugin
Language files within plugin directories not yet implemented.
Sort order on criteria.php page is still wrong.

lang/en/badges.php
@@ -105,11 +105,11 @@ $string['badgesalt_desc'] = 'Using a hash allows backpack services to confirm th
 $string['badgesdisabled'] = 'Badges are not enabled on this site.';
 $string['badgesearned'] = 'Number of badges earned: {$a}';
 $string['badgesettings'] = 'Badges settings';
-$string['badgestatus_0'] = 'Not available to users';
-$string['badgestatus_1'] = 'Available to users';
-$string['badgestatus_2'] = 'Not available to users';
-$string['badgestatus_3'] = 'Available to users';
-$string['badgestatus_4'] = 'Archived';
+$string['badgestatus_1'] = 'Not available to users';
+$string['badgestatus_2'] = 'Available to users';
+$string['badgestatus_3'] = 'Not available to users';
+$string['badgestatus_4'] = 'Available to users';
+$string['badgestatus_5'] = 'Archived';
 $string['badgestoearn'] = 'Number of badges available: {$a}';
 $string['badgesview'] = 'Course badges';
 $string['badgeurl'] = 'Issued badge link';
@@ -140,41 +140,41 @@ $string['criteriasummary'] = 'Criteria summary';
 $string['criteria_descr'] = 'Learners are awarded this badge when they complete the following requirement: ';
 $string['criteria_descr_bydate'] = ' by <em>{$a}</em> ';
 $string['criteria_descr_grade'] = ' with minimum grade of <em>{$a}</em> ';
-$string['criteria_descr_short0'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_short1'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_short2'] = 'Awarded by <strong>{$a}</strong> of: ';
-$string['criteria_descr_short4'] = 'Complete the course ';
-$string['criteria_descr_short5'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_short6'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_single_short1'] = 'Complete: ';
-$string['criteria_descr_single_short2'] = 'Awarded by: ';
-$string['criteria_descr_single_short4'] = 'Complete the course ';
-$string['criteria_descr_single_short5'] = 'Complete: ';
-$string['criteria_descr_single_short6'] = 'Complete: ';
-$string['criteria_descr_single_1'] = 'The following activity has to be completed:';
-$string['criteria_descr_single_2'] = 'This badge has to be awarded by a user with the following role:';
-$string['criteria_descr_single_4'] = 'Learners must complete the course ';
-$string['criteria_descr_single_5'] = 'The following course has to be completed:';
-$string['criteria_descr_single_6'] = 'The following user profile field has to be completed:';
-$string['criteria_descr_0'] = 'Learners are awarded this badge when they complete <strong>{$a}</strong> of the listed requirements.';
-$string['criteria_descr_1'] = '<strong>{$a}</strong> of the following activities are completed:';
-$string['criteria_descr_2'] = 'This badge has to be awarded by the users with <strong>{$a}</strong> of the following roles:';
-$string['criteria_descr_4'] = 'Learners must complete the course ';
-$string['criteria_descr_5'] = '<strong>{$a}</strong> of the following courses have to be completed:';
-$string['criteria_descr_6'] = '<strong>{$a}</strong> of the following user profile fields have to be completed:';
-$string['criteria_0'] = 'This badge is awarded when...';
-$string['criteria_1'] = 'Activity completion';
-$string['criteria_1_help'] = 'Allows a badge to be awarded to users based on the completion of a set of activities within a course.';
-$string['criteria_2'] = 'Manual issue by role';
-$string['criteria_2_help'] = 'Allows a badge to be awarded manually by users who have a particular role within the site or course.';
-$string['criteria_3'] = 'Social participation';
-$string['criteria_3_help'] = 'Social';
-$string['criteria_4'] = 'Course completion';
-$string['criteria_4_help'] = 'Allows a badge to be awarded to users who have completed the course. This criterion can have additional parameters such as minimum grade and date of course completion.';
-$string['criteria_5'] = 'Completing a set of courses';
-$string['criteria_5_help'] = 'Allows a badge to be awarded to users who have completed a set of courses. Each course can have additional parameters such as minimum grade and date of course completion. ';
-$string['criteria_6'] = 'Profile completion';
-$string['criteria_6_help'] = 'Allows a badge to be awarded to users for completing certain fields in their profile. You can select from default and custom profile fields that are available to users. ';
+$string['criteria_descr_short_overall'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_activity'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_manual'] = 'Awarded by <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_course'] = 'Complete the course ';
+$string['criteria_descr_short_courseset'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_profile'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_single_short_activity'] = 'Complete: ';
+$string['criteria_descr_single_short_manual'] = 'Awarded by: ';
+$string['criteria_descr_single_short_course'] = 'Complete the course ';
+$string['criteria_descr_single_short_courseset'] = 'Complete: ';
+$string['criteria_descr_single_short_profile'] = 'Complete: ';
+$string['criteria_descr_single_activity'] = 'The following activity has to be completed:';
+$string['criteria_descr_single_manual'] = 'This badge has to be awarded by a user with the following role:';
+$string['criteria_descr_single_course'] = 'Learners must complete the course ';
+$string['criteria_descr_single_courseset'] = 'The following course has to be completed:';
+$string['criteria_descr_single_profile'] = 'The following user profile field has to be completed:';
+$string['criteria_descr_overall'] = 'Learners are awarded this badge when they complete <strong>{$a}</strong> of the listed requirements.';
+$string['criteria_descr_activity'] = '<strong>{$a}</strong> of the following activities are completed:';
+$string['criteria_descr_manual'] = 'This badge has to be awarded by the users with <strong>{$a}</strong> of the following roles:';
+$string['criteria_descr_course'] = 'Learners must complete the course ';
+$string['criteria_descr_courseset'] = '<strong>{$a}</strong> of the following courses have to be completed:';
+$string['criteria_descr_profile'] = '<strong>{$a}</strong> of the following user profile fields have to be completed:';
+$string['criteria_overall'] = 'This badge is awarded when...';
+$string['criteria_activity'] = 'Activity completion';
+$string['criteria_activity_help'] = 'Allows a badge to be awarded to users based on the completion of a set of activities within a course.';
+$string['criteria_manual'] = 'Manual issue by role';
+$string['criteria_manual_help'] = 'Allows a badge to be awarded manually by users who have a particular role within the site or course.';
+$string['criteria_social'] = 'Social participation';
+$string['criteria_social_help'] = 'Social';
+$string['criteria_course'] = 'Course completion';
+$string['criteria_course_help'] = 'Allows a badge to be awarded to users who have completed the course. This criterion can have additional parameters such as minimum grade and date of course completion.';
+$string['criteria_courseset'] = 'Completing a set of courses';
+$string['criteria_courseset_help'] = 'Allows a badge to be awarded to users who have completed a set of courses. Each course can have additional parameters such as minimum grade and date of course completion. ';
+$string['criteria_profile'] = 'Profile completion';
+$string['criteria_profile_help'] = 'Allows a badge to be awarded to users for completing certain fields in their profile. You can select from default and custom profile fields that are available to users. ';
 $string['criterror'] = 'Current parameters issues';
 $string['criterror_help'] = 'This fieldset shows all parameters that were initially added to this badge requirement but are no longer available. It is recommended that you uncheck such parameters to make sure that learners can earn this badge in the future.';
 $string['currentimage'] = 'Current image';

Rav015/moodle-totara

lang/en/badges.php
@@ -105,11 +105,11 @@ $string['badgesalt_desc'] = 'Using a hash allows backpack services to confirm th
 $string['badgesdisabled'] = 'Badges are not enabled on this site.';
 $string['badgesearned'] = 'Number of badges earned: {$a}';
 $string['badgesettings'] = 'Badges settings';
-$string['badgestatus_0'] = 'Not available to users';
-$string['badgestatus_1'] = 'Available to users';
-$string['badgestatus_2'] = 'Not available to users';
-$string['badgestatus_3'] = 'Available to users';
-$string['badgestatus_4'] = 'Archived';
+$string['badgestatus_1'] = 'Not available to users';
+$string['badgestatus_2'] = 'Available to users';
+$string['badgestatus_3'] = 'Not available to users';
+$string['badgestatus_4'] = 'Available to users';
+$string['badgestatus_5'] = 'Archived';
 $string['badgestoearn'] = 'Number of badges available: {$a}';
 $string['badgesview'] = 'Course badges';
 $string['badgeurl'] = 'Issued badge link';
@@ -140,41 +140,41 @@ $string['criteriasummary'] = 'Criteria summary';
 $string['criteria_descr'] = 'Users are awarded this badge when they complete the following requirement:';
 $string['criteria_descr_bydate'] = ' by <em>{$a}</em> ';
 $string['criteria_descr_grade'] = ' with minimum grade of <em>{$a}</em> ';
-$string['criteria_descr_short0'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_short1'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_short2'] = 'Awarded by <strong>{$a}</strong> of: ';
-$string['criteria_descr_short4'] = 'Complete the course ';
-$string['criteria_descr_short5'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_short6'] = 'Complete <strong>{$a}</strong> of: ';
-$string['criteria_descr_single_short1'] = 'Complete: ';
-$string['criteria_descr_single_short2'] = 'Awarded by: ';
-$string['criteria_descr_single_short4'] = 'Complete the course ';
-$string['criteria_descr_single_short5'] = 'Complete: ';
-$string['criteria_descr_single_short6'] = 'Complete: ';
-$string['criteria_descr_single_1'] = 'The following activity has to be completed:';
-$string['criteria_descr_single_2'] = 'This badge has to be awarded by a user with the following role:';
-$string['criteria_descr_single_4'] = 'Users must complete the course';
-$string['criteria_descr_single_5'] = 'The following course has to be completed:';
-$string['criteria_descr_single_6'] = 'The following user profile field has to be completed:';
-$string['criteria_descr_0'] = 'Users are awarded this badge when they complete <strong>{$a}</strong> of the listed requirements.';
-$string['criteria_descr_1'] = '<strong>{$a}</strong> of the following activities are completed:';
-$string['criteria_descr_2'] = 'This badge has to be awarded by the users with <strong>{$a}</strong> of the following roles:';
-$string['criteria_descr_4'] = 'Users must complete the course';
-$string['criteria_descr_5'] = '<strong>{$a}</strong> of the following courses have to be completed:';
-$string['criteria_descr_6'] = '<strong>{$a}</strong> of the following user profile fields have to be completed:';
-$string['criteria_0'] = 'This badge is awarded when...';
-$string['criteria_1'] = 'Activity completion';
-$string['criteria_1_help'] = 'Allows a badge to be awarded to users based on the completion of a set of activities within a course.';
-$string['criteria_2'] = 'Manual issue by role';
-$string['criteria_2_help'] = 'Allows a badge to be awarded manually by users who have a particular role within the site or course.';
-$string['criteria_3'] = 'Social participation';
-$string['criteria_3_help'] = 'Social';
-$string['criteria_4'] = 'Course completion';
-$string['criteria_4_help'] = 'Allows a badge to be awarded to users who have completed the course. This criterion can have additional parameters such as minimum grade and date of course completion.';
-$string['criteria_5'] = 'Completing a set of courses';
-$string['criteria_5_help'] = 'Allows a badge to be awarded to users who have completed a set of courses. Each course can have additional parameters such as minimum grade and date of course completion. ';
-$string['criteria_6'] = 'Profile completion';
-$string['criteria_6_help'] = 'Allows a badge to be awarded to users for completing certain fields in their profile. You can select from default and custom profile fields that are available to users. ';
+$string['criteria_descr_short_overall'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_activity'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_manual'] = 'Awarded by <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_course'] = 'Complete the course ';
+$string['criteria_descr_short_courseset'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_short_profile'] = 'Complete <strong>{$a}</strong> of: ';
+$string['criteria_descr_single_short_activity'] = 'Complete: ';
+$string['criteria_descr_single_short_manual'] = 'Awarded by: ';
+$string['criteria_descr_single_short_course'] = 'Complete the course ';
+$string['criteria_descr_single_short_courseset'] = 'Complete: ';
+$string['criteria_descr_single_short_profile'] = 'Complete: ';
+$string['criteria_descr_single_activity'] = 'The following activity has to be completed:';
+$string['criteria_descr_single_manual'] = 'This badge has to be awarded by a user with the following role:';
+$string['criteria_descr_single_course'] = 'Users must complete the course ';
+$string['criteria_descr_single_courseset'] = 'The following course has to be completed:';
+$string['criteria_descr_single_profile'] = 'The following user profile field has to be completed:';
+$string['criteria_descr_overall'] = 'Learners are awarded this badge when they complete <strong>{$a}</strong> of the listed requirements.';
+$string['criteria_descr_activity'] = '<strong>{$a}</strong> of the following activities are completed:';
+$string['criteria_descr_manual'] = 'This badge has to be awarded by the users with <strong>{$a}</strong> of the following roles:';
+$string['criteria_descr_course'] = 'Users must complete the course ';
+$string['criteria_descr_courseset'] = '<strong>{$a}</strong> of the following courses have to be completed:';
+$string['criteria_descr_profile'] = '<strong>{$a}</strong> of the following user profile fields have to be completed:';
+$string['criteria_overall'] = 'This badge is awarded when...';
+$string['criteria_activity'] = 'Activity completion';
+$string['criteria_activity_help'] = 'Allows a badge to be awarded to users based on the completion of a set of activities within a course.';
+$string['criteria_manual'] = 'Manual issue by role';
+$string['criteria_manual_help'] = 'Allows a badge to be awarded manually by users who have a particular role within the site or course.';
+$string['criteria_social'] = 'Social participation';
+$string['criteria_social_help'] = 'Social';
+$string['criteria_course'] = 'Course completion';
+$string['criteria_course_help'] = 'Allows a badge to be awarded to users who have completed the course. This criterion can have additional parameters such as minimum grade and date of course completion.';
+$string['criteria_courseset'] = 'Completing a set of courses';
+$string['criteria_courseset_help'] = 'Allows a badge to be awarded to users who have completed a set of courses. Each course can have additional parameters such as minimum grade and date of course completion. ';
+$string['criteria_profile'] = 'Profile completion';
+$string['criteria_profile_help'] = 'Allows a badge to be awarded to users for completing certain fields in their profile. You can select from default and custom profile fields that are available to users. ';
 $string['criterror'] = 'Current parameters issues';
 $string['criterror_help'] = 'This fieldset shows all parameters that were initially added to this badge requirement but are no longer available. It is recommended that you un-check such parameters to make sure that users can earn this badge in the future.';
 $string['currentimage'] = 'Current image';